f69ad0d358d871183af0d425cd2b18ffe00b6684
Гаджимурад Багандов <muradb20398@gmail.com>
2021-06-13 21:48:21 +0300

    Тестовая среда

---
+++ WPFTests/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFTests.Commands;
+
+namespace WPFTests
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public ICommand command { get; set; }
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            command = new LambdaCommand((o) => { MessageBox.Show("1"); }); ;
+        }
+
+        private void ListBoxItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+    }
+}
