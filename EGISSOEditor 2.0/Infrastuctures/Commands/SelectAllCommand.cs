e6f702e86b28310b962cf12072e7608dc3ab669b
Гаджимурад Багандов <muradb20398@gmail.com>
2021-06-29 15:17:24 +0300

    Команда выделея всех файлов

---
+++ EGISSOEditor 2.0/Infrastuctures/Commands/SelectAllCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace EGISSOEditor_2._0.Infrastuctures.Commands
+{
+    internal class SelectAllCommand : Base.Command
+    {
+        public override bool CanExecute(object parameter)
+        {
+            if (!(parameter is ListBox listbox))
+                throw new ArgumentException();
+
+            return listbox.Items.Count > 0 && listbox.SelectedItems.Count != listbox.Items.Count;
+        }
+
+        public override void Execute(object parameter)
+        {
+            if (!(parameter is ListBox listbox))
+                throw new ArgumentException();
+
+            listbox.SelectAll();
+        }
+    }
+}
