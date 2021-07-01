2882685bd0494cc41fe8eebd1f7bf67ae26a4aed
Гаджимурад Багандов <muradb20398@gmail.com>
2021-07-01 11:30:03 +0300

    Сохранение конфигурации окна

--- EGISSOEditor 2.0/Views/Windows/MainWindow.xaml.cs
+++ EGISSOEditor 2.0/Views/Windows/MainWindow.xaml.cs
@@ -25,30 +25,41 @@ namespace EGISSOEditor_2._0
     /// </summary>
     public partial class MainWindow : Window
     {
-        private IFileRepository<EGISSOFile> _repository => App.Host.Services.GetRequiredService<IFileRepository<EGISSOFile>>();
-        private IUserDialog _userDialog => App.Host.Services.GetRequiredService<IUserDialog>();
-        private IRepositoryProcedureDialog<EGISSOFile> _repositoryDialog => App.Host.Services.GetRequiredService<IRepositoryProcedureDialog<EGISSOFile>>();
+        private IFileRepository<EGISSOFile> Repository => App.Host.Services.GetRequiredService<IFileRepository<EGISSOFile>>();
+        private IUserDialog UserDialog => App.Host.Services.GetRequiredService<IUserDialog>();
+        private IRepositoryProcedureDialog<EGISSOFile> RepositoryDialog => App.Host.Services.GetRequiredService<IRepositoryProcedureDialog<EGISSOFile>>();
 
         public MainWindow()
         {
             InitializeComponent();
+            Width = Properties.Settings.Default.MainWindowSize.Width;
+            Height = Properties.Settings.Default.MainWindowSize.Height;
+            Top = Properties.Settings.Default.MainWindowStartupLocation.Height;
+            Left = Properties.Settings.Default.MainWindowStartupLocation.Width;
+
+            if (Properties.Settings.Default.MainWindowMaximized)
+                WindowState = WindowState.Maximized;
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            var items = _repository.Items.Where(i => i.IsFileChanged == true).ToList();
+            var items = Repository.Items.Where(i => i.IsFileChanged).ToList();
 
             if (items.Count > 0)
             {
-                if (_userDialog.ShowMessage($"У вас есть несохраненные файлы! \n Вы действительно хотите выйти?", Title, ShowMessageIcon.Infomation, ShowMessageButtons.YesNo) 
+                if (UserDialog.ShowMessage($"У вас есть несохраненные файлы! \n Вы действительно хотите выйти?", Title, ShowMessageIcon.Infomation, ShowMessageButtons.YesNo)
                     == Services.Enums.DialogResult.No)
                 {
                     e.Cancel = true;
                     return;
                 }
             }
+            Properties.Settings.Default.MainWindowSize = new System.Drawing.Size((int)Width, (int)Height);
+            Properties.Settings.Default.MainWindowStartupLocation = new System.Drawing.Size((int)Left, (int)Top);
+            Properties.Settings.Default.MainWindowMaximized = WindowState == WindowState.Maximized;
+            Properties.Settings.Default.Save();
 
-            _repository.RemoveAll();      
+            Repository.RemoveAll();
         }
 
         private async void ListBoxCustom_Drop(object sender, DragEventArgs e)
@@ -57,7 +68,7 @@ namespace EGISSOEditor_2._0
             {
                 string[] files = ((string[])e.Data.GetData(DataFormats.FileDrop))
                     .Where(i => i.EndsWith(".xlsx")).ToArray();
-                await _repositoryDialog.AddWithShowProgressAsync(files);
+                await RepositoryDialog.AddWithShowProgressAsync(files);
             }
         }
     }
